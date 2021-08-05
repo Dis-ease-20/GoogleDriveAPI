79759db995353b45fe2c7c73e9bd8afb97749b6d
Vinita pal <pvinitak001@gmail.com>
2021-08-06 01:10:50 +0530

    Add files via upload

---
+++ PS2/main.js
@@ -0,0 +1,45 @@
+//  client id - 88089229711-el0le2p3eodlpvshu2utp4bgsgmorfsq.apps.googleusercontent.com
+// client sceret  -  1FJzkjHw_4nUMKSryOmMZHdU
+
+$(document).ready(function(){
+     
+    // client id of the project
+
+    var clientId = "88089229711-el0le2p3eodlpvshu2utp4bgsgmorfsq.apps.googleusercontent.com";
+
+    // redirect_uri of the project
+
+    var redirect_uri = "http://localhost/DriveUpload/upload.html";
+
+    // scope of the project
+
+    var scope = "https://www.googleapis.com/auth/drive";
+
+    // the url to which the user is redirected to
+
+    var url = "";
+
+
+    // this is event click listener for the button
+
+    $("#login").click(function(){
+
+       // this is the method which will be invoked it takes four parameters
+
+       signIn(clientId,redirect_uri,scope,url);
+
+    });
+
+    function signIn(clientId,redirect_uri,scope,url){
+     
+       // the actual url to which the user is redirected to 
+
+       url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri="+redirect_uri
+       +"&prompt=consent&response_type=code&client_id="+clientId+"&scope="+scope
+       +"&access_type=offline";
+
+       // this line makes the user redirected to the url
+
+       window.location = url;
+    }
+});
